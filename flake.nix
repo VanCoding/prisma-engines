bec4da3195df1ce40b2559939510abce2159b635
Patrik Stutz <patrik.stutz@gmail.com>
2021-03-18 00:33:16 +0100

    progress on nix-flake support

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in rec {
+      # `nix build`
+      packages.my-project = naersk-lib.buildPackage {
+        pname = "my-project";
+        root = ./.;
+        singleStep = true;
+        copyTarget = true;
+      };
+      defaultPackage = packages.my-project;
+    });
+}
